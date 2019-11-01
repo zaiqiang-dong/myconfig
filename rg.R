acb6bf7e5fe64ffc1eef82b1971381f832c2e271
Zaiqiang Dong <zqdong@nreal.ai>
2019-11-01 17:24:58 +0800

    u

--- rg.R
+++ rg.R
@@ -1,10 +1,3 @@
 ylst <- yaml::yaml.load_file('./open.yaml')
 dt <- data.tree::as.Node(ylst)
-collapsibleTree::collapsibleTree(
-		dt,
-		fill = 'orangered',
-		root = 'vfs_open',
-		width = 1850,
-		height = 900,
-		attribute = 'file',
-		tooltip = T)
+collapsibleTree::collapsibleTree(dt,fill = 'orangered',root = 'vfs_open',width = 1920,height = 1080,attribute = 'file',tooltip = T)
