c6c8927294beb3f3222dadd8dab11ef237fd9eeb
zqdong <zqdong@nreal.ai>
2019-11-29 16:14:18 +0800

    update

--- wificonnect.sh
+++ wificonnect.sh
@@ -1,1 +1,3 @@
+adb disconnect
+adb root
 adb tcpip 5555; sleep 1;adb shell "ip a" | grep inet[^6]|grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}' | grep "192" | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' |xargs  adb connect
