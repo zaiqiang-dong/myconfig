65577c94976edec6a84e970b15f6b70b80af919d
Zaiqiang Dong <zqdong@nreal.ai>
2019-12-03 20:08:32 +0800

    update

--- setupconfig.sh
+++ setupconfig.sh
@@ -2,11 +2,7 @@ cp -r ./back ~
 sudo apt-get install vim-gnome
 sudo apt-get install openjdk-8-jdk
 sudo apt-get install gnome-terminal
-sudo apt-get install nodejs-legacy node.js
-sudo apt-get install npm
-sudo apt-get install cflow
 sudo apt-get install ctags cscope
-sudo sudo apt-get install r-base
 sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip
 sudo apt-get install android-platform-tools-base
 sudo apt-get install android-tools-adb
