6e644b8aad7062993b4073f58e8db99c369f7c39
Zaiqiang dong <zqdong@nreal.ai>
2020-07-09 19:44:26 +0800

    update

--- qemu/kill-qemu.sh
+++ qemu/kill-qemu.sh
@@ -1,5 +1,25 @@
-pid=$(ps -e | grep "qemu")
-echo $pid
-tid=${pid%%pts*}
-echo $tid
-kill $tid
+idinfo=$(ps -e | grep "qemu" | awk '{print $1}')
+array=($idinfo)
+
+pid1=${array[0]}
+pid2=${array[1]}
+
+echo $pid1
+echo $pid2
+
+if [[ $1 == "a" ]]
+then
+    echo "kill "$pid1
+    kill $pid1
+    echo "kill "$pid2
+    kill $pid2
+else
+    if [ $pid1 -gt $pid2 ]
+    then
+        echo "kill "$pid1
+	kill $pid1
+    else
+        echo "kill "$pid2
+	kill $pid2
+    fi
+fi
