43f7a2d31ce65596bc382a15fee3265162cac725
Zaiqiang dong <zqdong@nreal.ai>
2020-07-15 11:34:26 +0800

    config samba

--- sambaconfig/smbc.sh
+++ sambaconfig/smbc.sh
@@ -1,13 +1,13 @@
 sudo apt-get install samba
 # sudo vi /etc/samba/smb.conf
 sudo chmod 777 /etc/samba/smb.conf
-echo "[samba]" >> /etc/samba/smb.conf
+echo "[zqdong]" >> /etc/samba/smb.conf
 echo "comment = Share Folder require password" >> /etc/samba/smb.conf
 echo "browseable = yes" >> /etc/samba/smb.conf
-echo "path = /samba" >> /etc/samba/smb.conf
+echo "path = /home/zqdong/" >> /etc/samba/smb.conf
 echo "create mask = 0777" >> /etc/samba/smb.conf
 echo "directory mask = 0777" >> /etc/samba/smb.conf
-echo "valid users = share" >> /etc/samba/smb.conf
+echo "valid users = zqdong" >> /etc/samba/smb.conf
 echo "force user = nobody" >> /etc/samba/smb.conf
 echo "force group = nogroup" >> /etc/samba/smb.conf
 echo "public = yes" >> /etc/samba/smb.conf
