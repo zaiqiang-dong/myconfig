c90d3633133c94290c65de60793d9c78f2361966
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-02 13:07:43 +0800

    update

--- lab/qemu-setup.sh
+++ lab/qemu-setup.sh
@@ -1,18 +1,47 @@
 
 if [ $1 = "arm64" ];then
     echo "run with arch=arm64"
-    qemu-system-aarch64 -m 1024 \
-			-machine virt -cpu cortex-a57 -machine type=virt \
+    qemu-system-aarch64 -m 1024 -smp 2 \
+ 			-machine virt -cpu cortex-a57 -machine type=virt \
 			-kernel $2/arm64-build-out/arch/arm64/boot/Image \
-			-initrd ../rootfs/arm64/initrd.gz \
-			--append "rdinit=/init console=ttyAMA0" \
+			-hda ../rootfs/arm64/sda-rootfs.img \
+			--append "rdinit=/init root=/dev/vda console=ttyAMA0" \
 			-nographic \
 			$3 $4
 fi
 
 if [ $1 = "x86_64" ];then
     echo "run with arch=x86_64"
-    qemu-system-x86_64  -m 1024 \
+    qemu-system-x86_64  -m 1024 --enable-kvm  -smp 1\
+			-kernel $2/x86_64-build-out/arch/x86_64/boot/bzImage \
+			-initrd ../rootfs/x86_64/initrd.gz \
+			-nographic \
+			--append "rdinit=/init console=ttyS0 nokaslr" \
+			$3 $4
+fi
+
+if [ $1 = "x86_64_Q" ];then
+    echo "run with arch=x86_64"
+    /home/dongzaiq/Downloads/qemu-5.2.0/build/qemu-system-x86_64 --enable-kvm -m 1024  -smp 1\
+			-kernel $2/x86_64-build-out/arch/x86_64/boot/bzImage \
+			-initrd ../rootfs/x86_64/initrd.gz \
+			--append "rdinit=/init console=ttyS0 nokaslr" \
+			$3 $4
+fi
+
+if [ $1 = "x86_64_G" ];then
+    echo "run with arch=x86_64"
+    cgdb --args /home/dongzaiq/Downloads/qemu-5.2.0/build/qemu-system-x86_64 --enable-kvm -m 1024  -smp 1\
+			-kernel $2/x86_64-build-out/arch/x86_64/boot/bzImage \
+			-initrd ../rootfs/x86_64/initrd.gz \
+			-nographic \
+			--append "rdinit=/init console=ttyS0 nokaslr" \
+			$3 $4
+fi
+
+if [ $1 = "x86_64_G_TCG" ];then
+    echo "run with arch=x86_64"
+    cgdb --args /home/dongzaiq/Downloads/qemu-5.2.0/build/qemu-system-x86_64 -m 1024  -smp 1\
 			-kernel $2/x86_64-build-out/arch/x86_64/boot/bzImage \
 			-initrd ../rootfs/x86_64/initrd.gz \
 			-nographic \
--- simplepush.sh
+++ simplepush.sh
@@ -1,6 +1,7 @@
 cp ~/.config/nvim/init.vim ./vim-config/nvim/init.vim
 cp ~/.config/nvim/coc-settings.json ./vim-config/nvim/coc-settings.json
 cp ~/.config/nvim/colors/mycolor.vim ./vim-config/nvim/colors/mycolor.vim
+cp ~/.config/zathura/zathurarc ./zathura-config/
 cp -r ~/.config/nvim/syntax ./vim-config/nvim/
 cp ~/.hyper.js ./hyper-config/.hyper.js
 cp ~/.zshrc ./shell-zsh/.zshrc
